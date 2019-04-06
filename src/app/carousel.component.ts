6abd5bf2d117012625cc59290ec8b49e4511c24a
lionder <lionder.97@bk.ru>
2019-04-07 01:29:09 +0300

    2 files modified

--- src/app/carousel.component.ts
+++ src/app/carousel.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngbd-carousel', 
-  templateUrl: './carousel.component.html'
+  templateUrl: './carousel.component.html',
+  styles: [`img{
+    max-width:100%;
+    height:auto;
+    max-height:100vh;
+  }
+  
+  ngb-carousel{
+    
+  }
+  `]
 })
 export class NgbdCarouselComponent {
-  images = [`https://firebasestorage.googleapis.com/v0/b/dreamsbreakfast-ab859.appspot.com/o/img%2FlogoWIP13.png?alt=media&token=d11748a0-9dcd-408b-bd8e-98c9b349abd9`];
+  images = [`https://firebasestorage.googleapis.com/v0/b/dreamsbreakfast-ab859.appspot.com/o/img%2FlogoWIP14-2.png?alt=media&token=01dba213-0dab-41a3-a951-ed768ac054a6`];
 }
